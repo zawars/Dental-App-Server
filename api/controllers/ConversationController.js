384b71df3b5b659fc92446dd638da4f1faef23d3
Zawar Shahid <zawar.shahid@dpworld.com>
2024-01-24 15:48:38 +0500

    api updated

--- api/controllers/ConversationController.js
+++ api/controllers/ConversationController.js
@@ -7,12 +7,12 @@
 
 module.exports = {
   getChatBoxData: async (req, res) => {
-    let conversations = await Conversation.find().limit(3).sort('createdAt desc').populateAll();
+    let conversations = await Conversation.find().limit(3).sort('updatedAt desc').populateAll();
     res.ok(conversations);
   },
 
   index: async (req, res) => {
-    let conversations = await Conversation.find().paginate({page: req.query.page, limit: 20}).sort('createdAt desc').populateAll();
+    let conversations = await Conversation.find().paginate({page: req.query.page, limit: 20}).sort('updatedAt desc').populateAll();
     res.ok(conversations);
   }
 
