e7d30d0599a45431f72954f2688ffd47e10dd096
Zawar Shahid <zawar.shahid@dpworld.com>
2024-01-25 21:12:24 +0500

    conversation api index updated for grouped messages

--- api/controllers/ConversationController.js
+++ api/controllers/ConversationController.js
@@ -12,7 +12,22 @@ module.exports = {
   },
 
   index: async (req, res) => {
-    let conversations = await Conversation.find().paginate({page: req.query.page, limit: 20}).sort('updatedAt desc').populateAll();
+    let conversations = await Conversation.find().paginate({
+      page: req.query.page,
+      limit: 20
+    }).sort('updatedAt desc').populateAll();
+
+    const groupedMessages = {};
+
+    conversations.messages.forEach(message => {
+      const date = message.timestamp.toISOString().split('T')[0]; // Extract date from timestamp
+      if (!groupedMessages[date]) {
+        groupedMessages[date] = [];
+      }
+      groupedMessages[date].push(message);
+    });
+
+    conversations.messages = groupedMessages;
     res.ok(conversations);
   }
 
